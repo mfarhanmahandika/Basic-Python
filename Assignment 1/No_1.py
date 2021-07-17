56d90232357c3e79d620f2994ad0a259fbd8a4b5
Muhammad Farhan Mahandika <farhan0632@gmail.com>
2021-07-17 14:04:07 +0700

    Nomor 1

---
+++ Assignment 1/No_1.py
@@ -0,0 +1,7 @@
+Nama = input("siapakah nama kamu?")
+Usia = int(input ("berapakah usiamu?"))
+Tinggi = float(input("berapa tinggi badanmu?"))
+
+Teks= "Hallo gaes, perkenalkan nama saya {}, umur saya {} dan tinggi badan saya {}". format(Nama, Usia, Tinggi)
+
+print(Teks)
